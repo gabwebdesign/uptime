d71f0d109f379fe6ee89f725cd7b33bd6068e19c
gabwebdesign <gab.webdesign@gmail.com>
2023-06-07 12:59:21 -0600

    cards

--- uptime-react/src/components/carduser.jsx/card.user.js
+++ uptime-react/src/components/carduser.jsx/card.user.js
@@ -2,10 +2,15 @@ import './style.scss';
 
 export const CardUser=({firtsName,lastName,state})=>{
     return(
-        <div className='card'>
-            <p>{firtsName}</p>
-            <p>{lastName}</p>
-            <p>{state}</p>
+        <div className='card-container'>
+            <div className='card d-flex justify-content-center'>
+                <img className='mb-3' src={require('../../assets/cards/profile.gif')}/>
+                <p className='text-uppercase text-center mb-0 fw-bolder'>{firtsName}  {lastName}</p>
+                <p className='text-center job-position'>Occupation</p>
+            </div>
+            <div className='card-container__state text-center d-flex justify-content-center align-items-center'>
+                <span>{state}</span>
+            </div>
         </div>
     )
 } 
--- uptime-react/src/components/dashboard/dashboard.jsx
+++ uptime-react/src/components/dashboard/dashboard.jsx
@@ -23,14 +23,22 @@ export const Dashboard =()=>{
     return(
         <div className='container-fluid'>
             <div className='row'>
-                <div className='dash d-flex flex-wrap justify-content-around w-100 p-3'>
-                    { totalUsers.map(((element)=>{
-                        return <CardUser 
-                        key={element[0]}
-                        firtsName={element[1].name}
-                        />
-                    }))
-                    }
+                <div className='d-flex flex-column w-100 p-3'>
+                    <div className='hero'>
+                        <h1 className='text-center pt-3 mb-1'>THE TEAM</h1>
+                        <p className='text-center mb-1'>____</p>
+                    </div>
+                    <div className='dash d-flex flex-wrap justify-content-around w-100 p-3'>
+                        { totalUsers.map(((element)=>{
+                            return <CardUser 
+                            key={element[0]}
+                            firtsName={element[1].name}
+                            lastName={element[1].lastName}
+                            state={element[1].state}
+                            />
+                        }))
+                        }
+                    </div>
                 </div>
             </div>
         </div>
